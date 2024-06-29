7a9daa412a9789c16462e0a64666b0c572227b80
priyadarshini122 <priyadarshinigaddala52git config user.email priyadarshinigaddala52@gmail.com>
2024-06-29 15:14:45 +0000

    Rock-Paper-Scissors

---
+++ src/GameResultView/index.js
@@ -0,0 +1,39 @@
+import {
+  ShowResultContainer,
+  ResultImagesContainer,
+  ResultTextImgContainer,
+  ResultText,
+  ResultImageItem,
+  ResultButtonContainer,
+  PlayAgainButton,
+} from './styledComponents'
+
+const GameResultView = props => {
+  const {mychoice, apponentChoice, resultMessage, playAgain} = props
+  const {id, image} = apponentChoice
+  const onClickPlayAgainButton = () => {
+    playAgain()
+  }
+  return (
+    <ShowResultContainer>
+      <ResultImagesContainer>
+        <ResultTextImgContainer>
+          <ResultText>YOU</ResultText>
+          <ResultImageItem src={mychoice[1]} alt='your choice' />
+        </ResultTextImgContainer>
+        <ResultTextImgContainer>
+          <ResultText>OPPONENT</ResultText>
+          <ResultImageItem src={image} alt='opponent choice' />
+        </ResultTextImgContainer>
+      </ResultImagesContainer>
+      <ResultText>{resultMessage}</ResultText>
+      <ResultButtonContainer>
+        <PlayAgainButton type='button' onClick={onClickPlayAgainButton}>
+          Play Again
+        </PlayAgainButton>
+      </ResultButtonContainer>
+    </ShowResultContainer>
+  )
+}
+
+export default GameResultView
